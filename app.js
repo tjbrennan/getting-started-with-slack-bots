310cdd72ec7773edafeb0eeaa2798f673f7a2fab
Taylor Brennan <taylorjbrennan@gmail.com>
2014-12-06 20:19:53 -0800

    implement hellobot; prepare for heroku deployment

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var hellobot = require('./hellobot');
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -11,6 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
 
+// hellobot
+app.post('/hello', hellobot);
 
 // basic error handler
 app.use(function (err, req, res, next) {
